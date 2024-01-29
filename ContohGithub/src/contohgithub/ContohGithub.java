88e04858db7148b56d5060a2d54859f02c5f7465
RawlsArdiansyah <fahzilaraul201@gmail.com>
2024-01-29 15:38:56 +0700

    Update ContohGithub.java

--- ContohGithub/src/contohgithub/ContohGithub.java
+++ ContohGithub/src/contohgithub/ContohGithub.java
@@ -15,6 +15,7 @@ public class ContohGithub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Raul & Daffa");
     }
     
 }
